adfc7cec705b238bc8963f0d848eff23de59fa49
GauravDarkslayer <www.gaurav10bhojwani@gmail.com>
2019-07-18 14:26:40 +0530

    now link it
    put  css files in static folder

--- main_community/views.py
+++ main_community/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-from
+from django.http import HttpResponse
+from .forms import Login,Signup
+from django.views import View
 # Create your views here.
+
+# def index(request):
+#     return render(request,'app1/header.html')
+
+def home(request):
+    return render(request,'colorlib-regform-7/index.html')
+
+
+def login(request):
+    form = Login()
+    return render(request,'colorlib-regform-7/login.html',{'form':form})
+
+def signup(request):
+    form = Signup()
+    return render(request,'colorlib-regform-7/signup.html',{'form':form})
+
+# class Signnedup(View):
+#     def get(self,request):
+#         error = 'Invalid method'
+#         form=Signup()
+#         return render(request,'app1/signup1/',{'form':form})
+#
+#     def signup(self,request):
+#         form=Signup(request.POST,request.FILES)
+#         if form.is_valid():
+#             p1 = form.cleaned_data()
+#         else:
+#             error = 'Invalid password try agian'
+#             form=Signup()
+#             return render(request,'app1/signup.html',{'form':form,'error':error})
