e012a0f96c6ca6192c597f2c5bce0c1cc3806d1d
Gauravdarkslayer <www.gaurav10bhojwani@gmail.com>
2019-08-20 15:02:45 +0530

    interest modified

--- main_community/views.py
+++ main_community/views.py
@@ -66,12 +66,12 @@ class Signnedup(View):
                         return render(request,"colorlib-regform-7/login.html",{'dict':dict})
                     else:
                         error = "Password does not match...Try again"
-                        form = Signup()
+                        # form = Signup()
                         #return HttpResponse('<h1>password not matched</h1>')
-                        return render(request,"app1/signup.html",{'form':form,'error':error})
+                        return render(request,"colorlib-regform-7/sign up.html",{'error':error})
                 else:
                     error = "User already exist..."
-                    form = Signup()
+                    # form = Signup()
                     # return HttpResponse('<h1>user already exist</h1>')
                     return render(request,"colorlib-regform-7/login.html",{'error':error})#'form':form,
 
@@ -127,29 +127,53 @@ def logout(request):
 
 # Function called in interest screen
 def getinterest(request):
+    
+    def insert(interest):
+        with connection.cursor() as cursor:
+            mail=request.session['email'] #Getting current user email
+            print(mail)
+            cursor.execute("select id from user where emailid='{}'".format(mail))
+            CURRENT_USER_ID=cursor.fetchone()
+            print(CURRENT_USER_ID)
+            cursor.execute("insert into getinterest (userid,interestid) values({},'{}')".format(*CURRENT_USER_ID,interest))
     INTEREST_PYTHON=request.POST.get('python') 
-    print("this is here",INTEREST_PYTHON)
+    #print("this is here",INTEREST_PYTHON)
     INTEREST_CPP=request.POST.get('c++') 
-    print("this is here",INTEREST_CPP)
+    #print("this is here",INTEREST_CPP)
     INTEREST_VFX=request.POST.get('vfx') 
-    print("this is here",INTEREST_VFX)
+    #print("this is here",INTEREST_VFX)
     INTEREST_JAVA=request.POST.get('java') 
-    print("this is here",INTEREST_JAVA)
+    #print("this is here",INTEREST_JAVA)
     INTEREST_ANDROID=request.POST.get('android') 
-    print("this is here",INTEREST_ANDROID)
+    #print("this is here",INTEREST_ANDROID)
     INTEREST_PHP=request.POST.get('php') 
-    print("this is here",INTEREST_PHP)
+    #print("this is here",INTEREST_PHP)
     INTEREST_NODEjs=request.POST.get('node') 
-    print("this is here",INTEREST_NODEjs)
+    #print("this is here",INTEREST_NODEjs)
     INTEREST_GO=request.POST.get('go') 
-    print("this is here",INTEREST_GO)
+    #print("this is here",INTEREST_GO)
     INTEREST_CSHARP=request.POST.get('csharp') 
-    print("this is here",INTEREST_CSHARP)
+    #print("this is here",INTEREST_CSHARP)
+    if INTEREST_PYTHON:
+        insert(INTEREST_PYTHON)
+    if INTEREST_CPP:
+        insert(INTEREST_CPP)
+    if INTEREST_VFX:
+        insert(INTEREST_VFX)
+    if INTEREST_JAVA:
+        insert(INTEREST_JAVA)
+    if INTEREST_ANDROID:
+        insert(INTEREST_ANDROID)
+    if INTEREST_PHP:
+        insert(INTEREST_PHP)
+    if INTEREST_NODEjs:
+        insert(INTEREST_NODEjs)
+    if INTEREST_GO:
+        insert(INTEREST_GO)
+    if INTEREST_CSHARP:
+        insert(INTEREST_CSHARP)
+    return render('<h1>Interest getted</h1>')
 
     # ml = request.POST.get('ML')
     # print("This is 2nd item",ml)
-    with connection.cursor() as cursor:
-        mail=request.session['email'] #Getting current user email
-        CURRENT_USER_ID=cursor.execute("select id from user where emailid='{}'".format(mail))
-        cursor.execute("insert into getinterest(userid,interestid) values('{}','{}')".format(CURRENT_USER_ID,INTEREST_PYTHON))
-    return HttpResponse('<h1>Interest getted</h1>')
+    
